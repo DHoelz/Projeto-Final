2ce0af5039502f5b336cddb902d7587c3b88209d
DHoelz <=>
2025-11-18 13:17:59 -0300

    - Atualização do arquivo conftest.py para o novo formato de chaves.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,9 @@ def pytest_configure():
     )
     os.environ.setdefault(
         "crypto_key_aes256",
-        "4f9c1b8ad4e5c632c31df07bebc2fa31ed923c211c4fd49e18d117a8cc962a4f",
+        "buKRQdINqMHI+DQQ6TgRl4BVFUgDQtffKl7lbQDE0ho=",
     )
     os.environ.setdefault(
         "crypto_key_chacha20",
-        "a3d5c8e42fb91bf8327bd49c80d0d63a8c0aef2fba6b571e2ac275f77cc8e942",
+        "ewKDn8wwrANK+I3g3zXiOtWGLyNiOSaVOUwfY5viDzY=",
     )
